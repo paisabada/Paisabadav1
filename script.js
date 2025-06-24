524d6f1797b9bdad768050ceef06fae5080a0c7c
paisabada <ravibhisikar2007@gmail.com>
2025-06-25 00:32:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 function openForm(productName) {
   document.getElementById('leadModal').style.display = 'block';
-  document.getElementById('productTitle').innerText = productName;
+  document.getElementById('productTitle').innerText = productName + " Inquiry";
+  document.getElementById('productSubject').value = "New Inquiry - " + productName;
 }
-
 function closeForm() {
   document.getElementById('leadModal').style.display = 'none';
 }
